f0e055854c383c500f1d9170120b5823f06b5eb0
Abdelrahman Mohammad <51163783+AbdelrahmanMohammad04@users.noreply.github.com>
2023-08-03 21:36:04 -0500

    Completed project

--- Project/index.js
+++ Project/index.js
@@ -1,45 +1,70 @@
-import { createInterface } from "node:readline/promises";
-import { stdin as input, stdout as output, env } from "node:process";
-import { Configuration, OpenAIApi } from "openai";
-import { read } from "node:fs";
+import { createInterface } from "node:readline/promises"
+import { stdin as input, stdout as output, env } from "node:process"
+import { Configuration, OpenAIApi } from "openai"
+import prompts from "prompts"
 
-const configuration = new Configuration({ apiKey: env.OPENAI_API_KEY });
-const openai = new OpenAIApi(configuration);
-const readline = createInterface({ input, output });
+const configuration = new Configuration({ apiKey: env.OPENAI_API_KEY })
+const openai = new OpenAIApi(configuration)
+const readline = createInterface({ input, output })
 
-const chatbotType = await readline.question(
-  "Hello, I am Abe, your AI debugger. What language are you using? "
-);
-const messages = [{ role: "system", content: chatbotType }];
-let userInput = await readline.question("Great! How may I assist you? ");
+const userInput = await readline.question(
+  "Hello, I am Abe, your AI debugger. How may I assist you? "
+)
+const messages = [{ role: "assistant", content: userInput }]
+let languageInput = await readline.question(`Great! What language are you using? `)
 
-async function testing(message) {
-    while (userInput !== ".exit") {
-        messages.push({ role: "user", content: message });
-        try {
-          const response = await openai.createChatCompletion({
-            messages,
-            model: "gpt-3.5-turbo",
-          });
-      
-          const botMessage = response.data.choices[0].message;
-          if (botMessage) {
-            messages.push(botMessage);
-            userInput = await readline.question("\n" + botMessage.content + "\n\n");
-          } else {
-            userInput = await readline.question("\nNo response, try asking again\n");
-          }
-        } catch (error) {
-          console.log(error.message);
-          userInput = await readline.question("\nSomething went wrong, try asking again\n");
+async function testing(message) {  
+  while (userInput !== ".exit") {
+      messages.push({ role: "user", content: `${userInput} ${message} in ${languageInput}` })
+      console.log(messages[messages.length-1].content)
+      try {
+        const response = await openai.createChatCompletion({
+          messages,
+          model: "gpt-3.5-turbo",
+        })
+    
+        const botMessage = response.data.choices[0].message
+        if (botMessage) {
+          messages.push(botMessage)
+          userInput = await readline.question("\n" + botMessage.content + "\n\n")
+        } else {
+          userInput = await readline.question("\nNo response, try asking again\n")
         }
+      } catch (error) {
+        console.log(error.message)
+        userInput = await readline.question("\nSomething went wrong, try asking again\n")
       }
-      readline.close()
+    }
+    readline.close()
 }
 
 try {
-    throw new Error("syntax error")
+  throw new Error(`#!/bin/bash
+
+  $PWD/python3-virtualenv/bin/python -m unittest discover -v tests/
+  
+  ramie@Rami-PC MINGW64 ~/MLH/portfolio/my-fork/mlh-sre-portfolio (main)
+  $ ./run-test.sh 
+  ./run-test.sh: line 3: /c/Users/ramie/MLH/portfolio/my-fork/mlh-sre-portfolio/python3-virtualenv/bin/python: No such file or directory`)
 } catch (error) {
+  let response = await prompts({
+    type: 'toggle',
+    name: 'detection',
+    message: 'Looks like you ran into an error, should I automatically detect?',
+    initial: true,
+    active: 'yes',
+    inactive: 'no'
+  })
+  if (response['detection']) {
     testing(error.message)
+  } else {
+    response = await prompts({
+      type: 'text',
+      name: 'manual',
+      message: 'Describe your error.'
+    })
+    console.log(response['manual'])
+    testing(response['manual'])
+  }
 }
 
